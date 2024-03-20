ee9cca1efb9e519ba908f6957ab0f8a3744198bd
Akshay-Padaliya <patel.akshay43@gmail.com>
2024-03-20 21:05:58 +0530

    HomeWork

--- src/Admin/Container/Category/Category.js
+++ src/Admin/Container/Category/Category.js
@@ -7,7 +7,6 @@ import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
 import { object, string, number, date, InferType } from 'yup';
 import { useFormik } from 'formik';
-import { TextFieldsTwoTone } from '@mui/icons-material';
 
 
 function Category(props) {
@@ -22,7 +21,7 @@ function Category(props) {
         setOpen(false);
     };
 
-    let contactSchema = object({
+    let categorySchema = object({
         category: string().required().matches(/^[a-zA-Z'-\s]*$/, 'Invalid name').min(2, 'use a valid name').max(15, 'use a valid name'),
         discription: string().required().min(10, 'Message is  too short')
     });
@@ -32,7 +31,7 @@ function Category(props) {
             category: '',
             discription: '',
         },
-        validationSchema: contactSchema,
+        validationSchema: categorySchema,
         onSubmit: values => {
             alert(JSON.stringify(values, null, 2));
         },
@@ -43,11 +42,11 @@ function Category(props) {
     return (
 
         <>
-            <React.Fragment>
+            <React.Fragment >
                 <div className='m-4 mx-5 d-flex justify-content-end'>
-                <Button variant="outlined" color='primary'  onClick={handleClickOpen}>
-                    Add Category
-                </Button>
+                    <Button variant="outlined" color='primary' onClick={handleClickOpen}>
+                        Add Category
+                    </Button>
                 </div>
                 <Dialog
                     open={open}
@@ -56,62 +55,78 @@ function Category(props) {
                         component: 'form',
                         onSubmit: (event) => {
                             event.preventDefault();
-                            // const formData = new FormData(event.currentTarget);
-                            // const formJson = Object.fromEntries(formData.entries());
-                            // const email = formJson.email;
-                            // console.log(email);
                             handleClose();
+                            handleSubmit();
                         },
                     }}
                 >
-                    <DialogTitle className='text-center'>Add Data</DialogTitle>
-                    <DialogContent>
-                    <div>
-                        <TextField
-                            autoFocus
-                            required
-                            margin="dense"
-                            name="category"
-                            label="Enter category"
-                            type="text"
-                            fullWidth
-                            variant="standard"
-                            onChange={handleChange}
-                            onBlur={handleBlur}
-                            value={values.category}
-                        />
-                        <span className='error'>{errors.category && touched.category ? errors.category : ''}</span>
-                    </div>
-                    <div>
-                        <TextField
-                            autoFocus
-                            required
-                            margin="dense"
-                            name= "discription"
-                            label="Enter category discription"
-                            type="text"
-                            fullWidth
-                            variant="standard"
-                            onChange={handleChange}
-                            onBlur={handleBlur}
-                            value={values.name}
-                        />
-                        <span className='error'>{errors.name && touched.name ? errors.name : ''}</span>
-                    </div>
-                    <div className='mb-4' >
-                                        <input
-                                            name='category'
-                                            type="text"
-                                            className="w-100 form-control border-0 py-3"
-                                            placeholder="Your Name"
-                                            onChange={handleChange}
-                                            onBlur={handleBlur}
-                                            value={values.category}
-                                        />
-                                        <span className='error'>{errors.category && touched.category ? errors.category : ''}</span>
-                                    </div>
+                    <DialogTitle className='text-center'>Add Category</DialogTitle>
+                    <DialogContent style={{width:500}}>
+                        {/* <div>
+                            <TextField
+                                autoFocus
+                                required
+                                margin="dense"
+                                name="category"
+                                label="Enter category"
+                                type="text"
+                                fullWidth
+                                variant="standard"
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                value={values.category}
+                            />
+                            <span className='error'>{errors.category && touched.category ? errors.category : ''}</span>
+                        </div>
+                        <div>
+                            <TextField
+                                autoFocus
+                                required
+                                margin="dense"
+                                name="discription"
+                                label="Enter category discription"
+                                type="text"
+                                fullWidth
+                                variant="standard"
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                                value={values.name}
+                            />
+                            <span className='error'>{errors.discription && touched.discription ? errors.discription : ''}</span>
+                        </div> */}
+
+                        <form onSubmit={handleSubmit} >
+                            <div className='mb-4' >
+                                <input
+                                    name='category'
+                                    type="text"
+                                    className="w-100 form-control border-1 py-3"
+                                    placeholder="Please enter category"
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    value={values.category}
+                                />
+                                <span className='error'>{errors.category && touched.category ? errors.category : ''}</span>
+                            </div>
+                            <div className='mb-4'>
+                                <textarea
+                                    name='discription'
+                                    className="w-100 form-control border-1"
+                                    rows={5} cols={10}
+                                    placeholder="Your Message"
+                                    defaultValue={""}
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                    value={values.discription}
+                                />
+                                <span className='error'>{errors.discription && touched.discription ? errors.discription : ''}</span>
+                            </div>
+
+                        </form>
                     </DialogContent>
                     <DialogActions>
+                        {/* <button className="btn form-control border-secondary py-2 mb-3 bg-white text-primary " onClick={handleClose} type="submit">Submit</button>
+                        <button className="btn form-control border-secondary py-2 mb-3 bg-white text-primary " onClick={handleClose} >Cancel</button> */}
                         <Button onClick={handleClose}>Cancel</Button>
                         <Button type="submit">Add</Button>
                     </DialogActions>
